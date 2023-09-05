b5969289a55e9de8d65d9d2962fa58fde172577d
jongsik22 <xxx_@kakao.com>
2023-09-05 10:28:55 +0900

    폴더생성

--- src/Q230904.java
+++ src/Q230904.java
@@ -1,20 +1,13 @@
 public class Q230904 {
     private static double correctCount = 0;
-    private static final String[][] TESTCASES = new String[][] {
-            { "MANGO", "mango" },
-            { "peach", "PEACH" },
-            { "Apple", "aPPLE" },
-            { "baNANa", "BAnanA" },
-            { "Believe In YourSelF", "bELIEVE iN yOURsELf" },
-            { "THiS is how LIFE is!!", "thIs IS HOW life IS!!" },
-            { "Li^^ve  p$osi&TIVE*", "lI^^VE  P$OSI&tive*" },
-            { "%tiMe~~~~ iS>.< gggggolD ?:)", "%TImE~~~~ Is>.< GGGGGOLd ?:)" }
-    };
+    private static final String[][] TESTCASES = new String[][] { { "MANGO", "mango" }, { "peach", "PEACH" },
+            { "Apple", "aPPLE" }, { "baNANa", "BAnanA" }, { "Believe In YourSelF", "bELIEVE iN yOURsELf" },
+            { "THiS is how LIFE is!!", "thIs IS HOW life IS!!" }, { "Li^^ve  p$osi&TIVE*", "lI^^VE  P$OSI&tive*" },
+            { "%tiMe~~~~ iS>.< gggggolD ?:)", "%TImE~~~~ Is>.< GGGGGOLd ?:)" } };
 
     public static void main(String[] args) {
         for (int i = 0; i < TESTCASES.length; i++)
-            System.out.println("Testcase " + i + " = " + test(TESTCASES[i][0],
-                    TESTCASES[i][1]));
+            System.out.println("Testcase " + i + " = " + test(TESTCASES[i][0], TESTCASES[i][1]));
 
         System.out.println("정답률 = " + (int) (correctCount / 8 * 100) + "%");
     }
